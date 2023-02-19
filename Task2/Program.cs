2bd0aa172035682157dba7e0e7f242e9cefc7982
Anna <shiskanovaag@mail.ru>
2023-02-19 13:03:07 +1000

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает 
+//диапазон возможных координат точек в этой четверти
+
+Console.WriteLine("Enter a number from 1 to 4");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i == 1)
+    Console.WriteLine("x > 0 and y > 0");
+else if (i == 2)
+    Console.WriteLine("x < 0 and y > 0");
+else if (i == 3)
+    Console.WriteLine("x < 0 and y < 0");
+else
+    Console.WriteLine("x > 0 and y < 0");
+ 
